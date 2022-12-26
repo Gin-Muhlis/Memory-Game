20fa58be22c8c8c1f2be74f3d62a9a1d98a40b55
Gin Gin Nurilham M <ginnncore717@gmail.com>
2022-12-26 21:06:49 +0700

    membuat logika untuk menjalankan memory game dengan paradigma oop

--- script/script.js
+++ script/script.js
@@ -70,7 +70,179 @@ class user {
   }
 }
 
+class audioController {
+  constructor() {
+    this.bgMusic = new Audio('../assets/audio/christmas.mp3');
+    this.flipSound = new Audio('../assets/audio/flip.wav');
+    this.matchSound = new Audio('../assets/audio/match.wav');
+    this.victorySound = new Audio('../assets/audio/victory.wav');
+    this.bgMusic.volume = 0.5;
+    this.bgMusic.loop = true;
+  }
 
+  startMusic() {
+    this.bgMusic.play();
+  }
+
+  stopMusic() {
+    this.bgMusic.pause();
+    this.bgMusic.currentTime = 0;
+  }
+
+  flip() {
+    this.flipSound.play();
+  }
+
+  match() {
+    this.matchSound.play();
+  }
+
+  victory() {
+    this.stopMusic()
+    this.victorySound.play();
+  }
+}
+
+class gameSystem {
+  constructor(card) {
+    this.cardsArray = card;
+    this.timer = 0;
+    this.second = 0;
+    this.minute = 0;
+    this.hour = 0;
+    this.hourText = document.getElementById('hour');
+    this.minuteText = document.getElementById('minute');
+    this.secondText = document.getElementById('second');
+    this.flipsText = document.getElementById('flips');
+    this.audio = new audioController();
+  }
+
+  startGame() {
+    this.matchedCards = [];
+    this.cardToCheck = null;
+    this.totalClicks = 0;
+    this.busy = true;
+
+    setTimeout(() => {
+      this.audio.startMusic();
+      this.shuffleCards(this.cardsArray);
+      this.counter = this.startCounter();
+      this.busy = false
+    }, 500)
+
+    this.hideCards();
+  }
+
+  startCounter() {
+    return setInterval(() => {
+      this.timer++;
+      this.second++;
+      this.secondText.innerText = `0${this.second}`;
+
+      if (this.second > 9) {
+        this.secondText.innerText = this.second;
+      }
+
+      if (this.second > 59) {
+        this.second = 0;
+        this.minute++;
+        this.minuteText.innerText = `0${this.minute}`;
+      }
+
+      if (this.minute > 9) {
+        this.minuteText.innerText = this.minute;
+      }
+
+      if (this.minute > 59) {
+        this.minute = 0;
+        this.hour++;
+        this.hourText.innerText = `0${this.hour}`;
+      }
+
+      if (this.hour > 9) {
+        this.hourText.innerText = `0${this.hour}`;
+      }
+    }, 1000)
+  }
+
+  canFlipCards(card) {
+    return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck; 
+  }
+
+  flipCards(card) {
+    if (this.canFlipCards(card)) {
+      this.audio.flip();
+      card.classList.add('visible');
+      this.totalClicks++;
+      this.flipsText.innerText = this.totalClicks;
+
+      if (this.cardToCheck) 
+        this.checkForMatch(card);
+      else 
+        this.cardToCheck = card; 
+    }
+       
+  }
+
+  shuffleCards(cardsArray) {
+    for (let i = cardsArray.length - 1; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      cardsArray[i].style.order = randomIndex;
+      cardsArray[randomIndex].style.order = i;
+    }
+  }
+
+  hideCards() {
+    this.cardsArray.forEach(card => {
+      card.classList.remove('visible');
+      card.classList.remove('matched');
+    })
+  }
+
+  getTypeCard(card) {
+    return card.getElementsByClassName('card-value')[0].src;
+  }
+
+  checkForMatch(card) {
+    if (this.getTypeCard(card) === this.getTypeCard(this.cardToCheck)) 
+      this.cardMatch(card, this.cardToCheck);
+    else 
+      this.cardMisMatch(card, this.cardToCheck);
+
+    this.cardToCheck = null;
+  }
+
+  cardMatch(card1, card2) {
+    this.audio.match();
+    this.matchedCards.push(card1);
+    this.matchedCards.push(card2);
+    card1.classList.add('matched');
+    card2.classList.add('matched');
+
+    if (this.matchedCards.length === this.cardsArray.length) {
+      this.victory();
+    }
+  }
+
+  cardMisMatch(card1, card2) {
+    this.busy = true;
+
+    setTimeout(() => {
+      card1.classList.remove('visible');
+      card2.classList.remove('visible');
+      this.busy = false;
+    }, 1000)
+  }
+
+  victory() {
+    clearInterval(this.counter)
+    this.audio.victory();
+    document.querySelector('.game-container').classList.remove('visible');
+    document.querySelector('.finish-section').classList.add('visible');
+    document.querySelector('.finish-victory').classList.add('visible');
+  }
+
+}
 
 // !mengecek ketika browser sedang loading
 if (document.readyState == 'loading') {
@@ -86,10 +258,13 @@ function ready() {
     document.querySelector('.sign-up-section').classList.add('visible');
   })
 
-  // !logika pengambilan data user
   const userGame = new user();
+  const cards = Array.from(document.getElementsByClassName('card'));
+  const game = new gameSystem(cards);
+
   userGame.setGender();
   userGame.setAvatar();
+
   
   // !menambahkan event pada button start game untuk memulai game
   const buttonsStartGame = Array.from(document.getElementsByClassName('start'));
@@ -99,13 +274,24 @@ function ready() {
         userGame.setUsername();
         if (userGame.getUsername() && userGame.getGender() && userGame.getGenderIcon() && userGame.getAvatar()) {
           document.querySelector('.sign-up-section').classList.remove('visible');
+          document.querySelector('.game-container').classList.add('visible');
+          game.startGame();
         } else {
           alert('Silahkan isi data diri terlebih dahulu');
         }
       } else if (element.classList.contains('start-again')) {
         document.querySelector('.finish-section').classList.remove('visible');
+        document.querySelector('.finish-victory').classList.remove('visible');
+        document.querySelector('.game-container').classList.add('visible');
+        game.startGame();
       }
     })
   })
 
+  // !menjalankan logika dari memory game
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      game.flipCards(card);
+    })
+  })
 }
